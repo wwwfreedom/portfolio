047434a2fe9319ac482068214b4700be1e009e6c
Kevin <tig3r86@gmail.com>
2015-05-19 21:44:41 +0930

    part 7 smooth scroll done

--- _site/assets/js/functions.js
+++ _site/assets/js/functions.js
@@ -1,5 +1,21 @@
 $( document ).ready(function() {
-
-  //
+  smoothScroll(400);
 
 });
+
+// to make the internal link of the one page site scroll smoothly instead of the default jump
+function smoothScroll(duration) {
+  // looks in the documents for all of the anchor tag with a href that has a # in it the key is the ^ sign.
+  $('a[href^="#"]').on('click', function(event){
+    // store the attribute of the link that was clicked in a variable
+    var target = $( $(this).attr('href'));
+
+    // so if attribute is not just a blank # then animate the body to scroll there
+    if(target.length){
+      event.preventDefault();
+      $('html, body').animate({
+        scrollTop: target.offset().top
+      }, duration);
+    }
+  });
+}
