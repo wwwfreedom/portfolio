93440d209b328daf8049cbe3e9dddcc1e79e0f1a
Kevin <tig3r86@gmail.com>
2015-05-22 18:13:42 +0930

    part 17 client section partial complete

--- _site/assets/js/functions.js
+++ _site/assets/js/functions.js
@@ -2,6 +2,7 @@ $( document ).ready(function() {
   smoothScroll(400);
   workBelt();
   workLoad();
+  clientLogic();
 
 });
 
@@ -55,4 +56,61 @@ function workLoad() {
     $('.project-load').html(spinner).load(newHTML);
     $('.project-title').text(newTitle);
   });
+}
+
+function clientLogic() {
+  // look for all the client unit and select the first one and assign an active-client class to it
+  $('.client-unit').first().addClass('active-client');
+  $('.client-logo').first().addClass('active-client');
+
+
+  // this anonymous function is to control the click of logo and changes the client testimonials
+  $('.client-logo').click(function() {
+    // store the instance of this so you can refer to the current object this correctly later
+    var self = $(this);
+    // store the sibblings of the current .client-logo by going up the parent and select the children
+    var siblings = self.parent().children();
+    // store the position of the children
+    var position = siblings.index(self);
+
+    // remove the class 'active-client' from all .client unit that you find, and find the position (eq) of the logo that you just clicked
+    $('.client-unit').removeClass('active-client').eq(position).addClass('active-client');
+    // same as above but for the logo
+    // $('.client-logo').removeClass('active-client').eq(position).addClass('active-client');
+    // A BETTER WAY
+    // so the sibling variable already store all the children within the div with class .client-logos , so you just use that to remove
+    siblings.removeClass('active-client');
+    // add the class active client to current selected logo which is the one you just clicked
+    self.addClass('active-client');
+  });
+
+
+    // function for next button behaviour
+    $('.client-control-next, .client-control-prev').click(function() {
+      var self = $(this),
+          curActiveClient = $('.clients-belt').find('.active-client'),
+          // look through all the children of the div clients-belt and store the index of the curActiveClient
+          position = $('.clients-belt').children().index(curActiveClient),
+          clientNum = $('.client-unit').length;
+
+      if(self.hasClass('client-control-next')) {
+
+        if(position < clientNum -1) {
+        // next here switch over to the next sibbling in the dom
+        $('.active-client').removeClass('active-client').next().addClass('active-client');
+        } else {
+          // otherwise find remove active-client from all elments in the dom in div client belt and after that add active-client class to the first dom element in that div.
+          $('.client-unit').removeClass('active-client').first().addClass('active-client');
+          $('.client-logo').removeClass('active-client').first().addClass('active-client');
+        }
+      } else {
+
+        if(position === 0) {
+          $('.client-unit').removeClass('active-client').last().addClass('active-client');
+          $('.client-logo').removeClass('active-client').last().addClass('active-client');
+        } else {
+          $('.active-client').removeClass('active-client').prev().addClass('active-client');
+        }
+      }
+    });
 }
